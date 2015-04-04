49666e5c2ad5eb0afa1cf7f82a6bbde48d60c5e0
Austin <Arnolra0@sewanee.edu>
2015-04-03 22:10:08 -0500

    Attempted Update

---
+++ Pictures.js
@@ -0,0 +1,8 @@
+var pictureArrayU = ['<img src="SewaneePictures/Fulford/IMG_0378.jpg" class="pic">',
+                     '<img src="SewaneePictures/McClurg/IMG_0355.jpg" class="pic">',
+                     '<img src="SewaneePictures/AllSaints/IMG_0358.jpg" class="pic">',
+                     '<img src="SewaneePictures/BookStore/IMG_0323.jpg" class="pic">',
+                     '<img src="SewaneePictures/Breslin/IMG_0249.jpg" class="pic">',
+                     '<img src="SewaneePictures/Breslin/IMG_0249.jpg" class="pic">',
+                     '<img src="SewaneePictures/Breslin/IMG_0249.jpg" class="pic">',
+                     '<img src="SewaneePictures/Fowler/IMG_0266.jpg" class="pic">'];
