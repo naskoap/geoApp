fbb42ac6d74cbc5f213411525b3a60d99bd4caef
naskoap <naskoap@gmail.com>
2015-04-08 17:28:37 -0500

    Update Pictures.js

--- Pictures.js
+++ Pictures.js
@@ -1,8 +1,8 @@
-var pictureArrayU = ['<img src="SewaneePictures/Fulford/IMG_0378.jpg" class="pic">',
-                     '<img src="SewaneePictures/McClurg/IMG_0355.jpg" class="pic">',
-                     '<img src="SewaneePictures/AllSaints/IMG_0358.jpg" class="pic">',
-                     '<img src="SewaneePictures/BookStore/IMG_0323.jpg" class="pic">',
-                     '<img src="SewaneePictures/Breslin/IMG_0249.jpg" class="pic">',
-                     '<img src="SewaneePictures/Breslin/IMG_0249.jpg" class="pic">',
-                     '<img src="SewaneePictures/Breslin/IMG_0249.jpg" class="pic">',
-                     '<img src="SewaneePictures/Fowler/IMG_0266.jpg" class="pic">'];
+var pictureArrayU = ['<img src="SewaneePictures/Fulford/IMG_0378.JPG" class="pic">',
+                     '<img src="SewaneePictures/McClurg/IMG_0355.JPG" class="pic">',
+                     '<img src="SewaneePictures/AllSaints/IMG_0358.JPG" class="pic">',
+                     '<img src="SewaneePictures/BookStore/IMG_0323.JPG" class="pic">',
+                     '<img src="SewaneePictures/Breslin/IMG_0249.JPG" class="pic">',
+                     '<img src="SewaneePictures/Breslin/IMG_0249.JPG" class="pic">',
+                     '<img src="SewaneePictures/Breslin/IMG_0249.JPG" class="pic">',
+                     '<img src="SewaneePictures/Fowler/IMG_0266.JPG" class="pic">'];
