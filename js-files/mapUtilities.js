d322dde9eecf1d99647ab9935986fdb7fd62e257
naskoap <naskoap@gmail.com>
2016-04-21 15:17:13 -0500

    Added a temperature feature

--- js-files/mapUtilities.js
+++ js-files/mapUtilities.js
@@ -96,6 +96,29 @@ function resetDirections(elementId, val) {
   elt.value = val;
 }
 
+$(document).ready(function() {
+  getWeather(); //Get the initial weather.
+  setInterval(getWeather, 600000); //Update the weather every 10 minutes.
+});
+
+function getWeather() {
+  $.simpleWeather({
+    location: '37383',
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';
+
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+}
+
 function resetAll() {
 
   cnt = 0;
